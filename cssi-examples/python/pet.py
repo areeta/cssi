010ceedfbc06a59a16ccf4d5b4075c17edf980ff
Areeta Wong <areetaw@gmail.com>
2018-07-20 16:09:34 -0700

    python lesson on objects and classes

--- cssi-examples/python/pet.py
+++ cssi-examples/python/pet.py
@@ -9,15 +9,23 @@ dog = {
 
 # Object - designing a template for an object
 class Dog(object):
+
+    # Main constructor
     def __init__(self, name, age):
+        # Objects has properties
         self.name = name
         self.age = age
         self.is_hungry = True
         self.needs_a_walk = True
         self.is_sleepy = False
 
+    def __str__(self):
+        return "%s who is %s" % (self.name, self.age)
+
+    # Objects has methods
     def feed(self):
         self.is_hungry = False
+        self.needs_a_walk = True
         print("Nomnomnom")
 
     def walk(self):
@@ -28,8 +36,13 @@ class Dog(object):
 
     # Write a function to let your dog sleep + wake up refreshed and hungry
     def sleep(self):
-        self.is_sleepy = False
-        self.is_hungry = True
+        if self.is_sleepy == True:
+            self.is_sleepy = False
+            self.is_hungry = True
+            print("ZzzZZzzZZ")
+
+    def play(self, other_dog):
+        print("%s is playing with %s." % (self.name, other_dog.name))
 
 dog1 = Dog("Doggy McDogface", 2)
 dog2 = Dog("Buster", 1)
@@ -43,4 +56,14 @@ print(type(dog1))
 dog1.feed()
 dog1.walk()
 dog1.walk()
+dog1.sleep()
+dog1.feed()
+dog1.walk()
+dog1.sleep()
+
 print dog1.is_hungry
+print(dog1)
+print(dog2)
+
+dog1.play(dog2)
+dog2.play(dog1)
