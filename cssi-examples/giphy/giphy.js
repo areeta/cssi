31fabf178d503bdc5d7e56859de3ad41ee927e44
Areeta Wong <areetaw@gmail.com>
2018-07-20 09:24:57 -0700

    day 2 with python

--- cssi-examples/giphy/giphy.js
+++ cssi-examples/giphy/giphy.js
@@ -2,11 +2,14 @@ const apiKey = "D9ra05ehCCnqpaPMsWU6GrFLdOxWGhtf";
 
 console.log("Hello");
 
+// This is the base URL for the Giphy API.
 let url = "http://api.giphy.com/v1/gifs/search";
+
 // `` (back tick) lets you combine strings
 let query = `${url}?api_key=${apiKey}&q=kittens&limit=1`;
 console.log(query);
 
+// This function fetches a gif that matches the given searchTerm.
 function fetchGif(searchTerm) {
   console.log(searchTerm);
   //this will go get information from another page
@@ -16,8 +19,8 @@ function fetchGif(searchTerm) {
   }).then(json => {
     let results = json.data;
     let result = results[0];
-    createImage(result.url);
-    console.log(result);
+    let imageUrl = result.images.downsized.url;
+    createImage(imageUrl);
   });
 };
 
