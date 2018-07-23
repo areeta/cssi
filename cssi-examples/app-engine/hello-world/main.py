bf069131b97f5965600aa0f9a8ed742d2a4dd33c
Areeta Wong <areetaw@gmail.com>
2018-07-23 10:56:13 -0700

    app engine with html

--- cssi-examples/app-engine/hello-world/main.py
+++ cssi-examples/app-engine/hello-world/main.py
@@ -2,8 +2,35 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello, World!")
+        self.response.write("<h1>Hello, World!</h1>")
+        self.response.write("<i>Hello, World! We have learned about:</i>")
+        self.response.write("<ul></ul>")
+        self.response.write("<li>HTML</li>")
+        self.response.write("<li>CSS</li>")
+        self.response.write("<li>Javascript</li>")
+        self.response.write("<li>Python</li>")
+        self.response.write("<li>Command Line</li>")
+        self.response.write("<li>GitHub</li>")
+        self.response.write("<li>AppEngine</li>")
+        self.response.write("<ul></ul>")
+        self.response.write("<a href='/bye'>Goodbye</a>")
+
+class SecretEntrance(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Shhh, this is a secret!")
+
+class GoodBye(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Bye Bye!")
+
+class About(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello! This is YAMLLLLLLLLL!")
 
 app = webapp2.WSGIApplication([
     ("/", MainPage),
+    ("/secret", SecretEntrance),
+    ("/bye", GoodBye),
+    ("/about", About),
+
 ], debug=True)
