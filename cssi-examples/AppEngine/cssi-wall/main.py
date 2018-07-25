0865f2dca464b7b298bc12304446345a5cb0c338
Areeta Wong <areetaw@gmail.com>
2018-07-25 16:52:10 -0700

    added comments to main.py

--- cssi-examples/AppEngine/cssi-wall/main.py
+++ cssi-examples/AppEngine/cssi-wall/main.py
@@ -9,7 +9,6 @@ env = jinja2.Environment(
     extensions=["jinja2.ext.autoescape"],
     autoescape=True)
 
-# Handlers
 class Message(ndb.Model):
     name = ndb.StringProperty()
     content = ndb.StringProperty()
@@ -17,18 +16,33 @@ class Message(ndb.Model):
 
 class Wall(webapp2.RequestHandler):
     def get(self):
-        message_query = Message.query().order(Message.created_time)
-        messages = message_query.fetch()
+    # Common Things to Do with a Handler
+
+        # 1. Get info from the user request
+            # We don't need to here
+
+        # 2. Read from (WE DID THIS) or write to the database
+        messages = Message.query().order(-Message.created_time).fetch()
+
+        # 3. Render a response back to user or computer
         templatesVar = {
             "messages": messages,
         }
         template = env.get_template("templates/wall.html")
         self.response.write(template.render(templatesVar))
+
     def post(self):
+    # Common Things to Do with a Handler
+
+        # 1. Get info from the user request
         name = self.request.get("name")
         content = self.request.get("content")
+
+        # 2. Read from or write (WE DID THIS) to the database
         newMessage = Message(name=name, content=content)
         newMessage.put()
+
+        # 3. Render a response back (OR RELOAD TO SOMEWHERE ELSE) to user or computer
         self.redirect("/")
 
 app = webapp2.WSGIApplication([
